031dfe8bcae6da27946ff2bd3e1e64a1693fec7e
DallingtonAsin <asingwire50dallington@gmail.com>
2023-02-23 11:52:50 +0300

    started on upgrading react native from 0.70.5 to 0.71.3

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import type {PropsWithChildren} from 'react';
 import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider as AppProvider } from 'react-native-paper';
--- app/src/configs/images.js
+++ app/src/configs/images.js
@@ -1,8 +1,5 @@
 export const images = {
-   logo: require('../../assets/images/logo.png'),
+   logo: require('../../assets/images/logo.jpg'),
    otpImage: require('../../assets/images/otp.webp'),
-   previous: require('../../assets/images/previous.png'),
-   next: require('../../assets/images/next.png'),
-   profileImage: 'https://www.shutterstock.com/shutterstock/photos/406022083/display_1500/stock-vector-beautiful-african-american-woman-avatar-profile-flat-illustration-406022083.jpg',
    no_information: require('../../assets/images/no_information.png'),
 }
