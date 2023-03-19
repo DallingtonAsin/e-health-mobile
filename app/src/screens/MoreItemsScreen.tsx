0ed931684a3ce34c56a91319445ddc08d4fafeff
DallingtonAsin <asingwire50dallington@gmail.com>
2023-03-19 16:57:09 +0300

    changed settings screen

--- app/src/screens/MoreItemsScreen.tsx
+++ app/src/screens/MoreItemsScreen.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { SafeAreaView, View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import * as config from '../configs'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Icon5 from 'react-native-vector-icons/FontAwesome5';
 import Toast from 'react-native-simple-toast';
+import { Context as AppContext } from '../context/appContext';
 import * as configs from '../configs';
 import CustomStackHeader from '../components/CustomStackHeader';
 import { getAppVersion } from '../components/common/SharedHelper';
@@ -13,19 +14,21 @@ const MoreItemsScreen = ({ navigation }: { navigation: any }) => {
 
     const listItems = [
         { id: 1, name: 'Profile Information', icon: 'user-circle', isIcon5: true, action: () => navigation.navigate('Profile') },
-        { id: 2, name: 'Settings', icon: 'cog', isIcon5: true, action: () => navigation.navigate('Settings') },
-        { id: 3, name: 'Notification Inbox', icon: 'envelope', isIcon5: true, action: () => navigation.navigate('Notifications') },
-        { id: 4, name: 'Help & Support', icon: 'question-circle', isIcon5: true, action: () => navigation.navigate('ContactUs') },
-        { id: 5, name: 'Terms & Conditions', icon: 'files-o', isIcon5: false, action: () => navigation.navigate('TermsConditions') },
-        { id: 6, name: 'About Us', icon: 'info-circle', isIcon5: false, action: () => navigation.navigate('AboutUs') },
-        { id: 7, name: 'Rate Us', icon: 'star', isIcon5: true, action: () => Toast.show('Coming soon...', Toast.LONG) },
+        { id: 2, name: 'Notification Inbox', icon: 'envelope', isIcon5: true, action: () => navigation.navigate('Notifications') },
+        { id: 3, name: 'Help & Support', icon: 'question-circle', isIcon5: true, action: () => navigation.navigate('ContactUs') },
+        { id: 4, name: 'Terms & Conditions', icon: 'files-o', isIcon5: false, action: () => navigation.navigate('TermsConditions') },
+        { id: 5, name: 'About Us', icon: 'info-circle', isIcon5: false, action: () => navigation.navigate('AboutUs') },
+        { id: 6, name: 'Rate Us', icon: 'star', isIcon5: true, action: () => Toast.show('Coming soon...', Toast.LONG) },
+        { id: 7, name: 'Signout', icon: 'power-off', isIcon5: true, action: () => signout() },
     ];
+
+    const { signout } = useContext(AppContext);
 
     const Item = ({ item }: { item: any }) => (
         <TouchableOpacity style={styles.item} onPress={item.action}>
-            { item.isIcon5
-                    ? <Icon5 name={item.icon} size={20} color={configs.colors.primary} />
-                    : <Icon name={item.icon} size={20} color={configs.colors.primary} />
+            {item.isIcon5
+                ? <Icon5 name={item.icon} size={20} color={configs.colors.primary} />
+                : <Icon name={item.icon} size={20} color={configs.colors.primary} />
             }
             <Text style={styles.itemTitle}>{item.name}</Text>
         </TouchableOpacity>
@@ -36,23 +39,23 @@ const MoreItemsScreen = ({ navigation }: { navigation: any }) => {
     );
 
     return (
-            <SafeAreaView style={styles.container}>
-                <CustomStackHeader title={'Preferences'} onPress={() => { navigation.goBack() }} />
-                <View style={styles.body}>
-                    <FlatList
-                        data={listItems}
-                        renderItem={renderItem}
-                        keyExtractor={(item: any, index: number) => item.id.toString()}
-                        showsVerticalScrollIndicator={false}
-                        showsHorizontalScrollIndicator={false}
-                        scrollEnabled={true}
-                        ListFooterComponent={<View style={{ height: 40 }} />}
-                    />
-                </View>
-                <View style={styles.footer}>
-                    <Text style={styles.versionText}>Current Version: {getAppVersion()}</Text>
-                </View>
-            </SafeAreaView>
+        <SafeAreaView style={styles.container}>
+            <CustomStackHeader title={'Preferences'} onPress={() => { navigation.goBack() }} />
+            <View style={styles.body}>
+                <FlatList
+                    data={listItems}
+                    renderItem={renderItem}
+                    keyExtractor={(item: any, index: number) => item.id.toString()}
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}
+                    scrollEnabled={true}
+                    ListFooterComponent={<View style={{ height: 40 }} />}
+                />
+            </View>
+            <View style={styles.footer}>
+                <Text style={styles.versionText}>Current Version: {getAppVersion()}</Text>
+            </View>
+        </SafeAreaView>
     )
 
 
